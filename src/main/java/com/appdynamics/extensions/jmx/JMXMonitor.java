d06c298157d007eb816aeee680b7d502837450a8
Bhuvnesh Kumar <raghur171@gmail.com>
2018-12-13 15:38:41 -0800

    adding initial changes

--- src/main/java/com/appdynamics/extensions/jmx/JMXMonitor.java
+++ src/main/java/com/appdynamics/extensions/jmx/JMXMonitor.java
@@ -116,7 +116,7 @@ public class JMXMonitor extends ABaseMonitor {
     public static void main(String[] args) throws TaskExecutionException {
         JMXMonitor JMXMonitor = new JMXMonitor();
         Map<String, String> argsMap = new HashMap<String, String>();
-        argsMap.put("config-file", "/Users/bhuvnesh.kumar/repos/appdynamics/extensions/jmx-monitoring-extension/src/test/resources/conf/config.yml");
+        argsMap.put("config-file", "/src/test/resources/conf/config.yml");
         JMXMonitor.execute(argsMap, null);
     }
 }
